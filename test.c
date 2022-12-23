55d69a8842e8fe853b80af37dde60ec6f7d1ee7b
Jan Paul Batrina <jmbatrina@up.edu.ph>
2022-12-23 15:40:49 +0800

    Add loop.c and test.c from Lab for schedlog testing
    Adapted from Lab 5 Section 2.1.1 schedlog syscall.
    Placed in separate commit so that in can be easily reverted
    so that testing files are not included in final branch

---
+++ test.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "user.h"
+
+int main() {
+    schedlog(10000);
+
+    // TODO: investigate why pid of first loop proc becomes very high (592)
+    // AFTER running usertests.c. This does not happen when compared to Lab 5 baseline
+    for (int i = 0; i < 3; i++) {
+        if (fork() == 0) {
+            char *argv[] = {"loop", 0};
+            exec("loop", argv);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        wait();
+    }
+
+    shutdown();
+}
